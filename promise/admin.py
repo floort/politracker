a510ad3f1b7985b5bee711955f58995f339689ad
Floor Terra <floort@gmail.com>
2011-04-30 01:12:32 +0200

    Made rudimentary model

---
+++ promise/admin.py
@@ -0,0 +1,7 @@
+from promise.models import *
+from django.contrib import admin
+
+admin.site.register(Party)
+admin.site.register(Source)
+admin.site.register(Politician)
+admin.site.register(Promise)
--- promise/models.py
+++ promise/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Party(models.Model):
+    name = models.CharField(max_length=64)
+    full_name = models.CharField(max_length=256, blank=True)
+
+
+class Politician(models.Model):
+    name = models.CharField(max_length=128)
+    party = models.ForeignKey(Party)
+
+class Source(models.Model):
+    slug = models.SlugField()
+    url = models.URLField()
+    date = models.DateTimeField(auto_now=True)
+
+class Promise(models.Model):    
+    slug = models.SlugField()
+    title = models.CharField(max_length=128)
+    open_date = models.DateTimeField(auto_now=True)
+    resolved_date = models.DateTimeField()
+    sources = models.ManyToManyField(Source)
+
+
